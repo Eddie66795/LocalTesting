c6be576d88765fb8ba944542fc71c6f7d33cfe0a
Eduard le Roux <eduard.leroux@jumo.world>
2018-05-08 08:34:11 +0200

    Adding local with TimeZone logic

--- src/Locale/LocaleMain.java
+++ src/Locale/LocaleMain.java
@@ -1,13 +1,26 @@
 package Locale;
 
+import java.text.NumberFormat;
 import java.util.Locale;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
 
 public class LocaleMain {
 
     public LocaleMain() {
         println("LocaleMain: Start");
-        local1();
+        local2();
         println("LocaleMain: End");
+    }
+
+    void getLocalList() {
+        Locale[] localeList = NumberFormat.getAvailableLocales();
+        println("Test: " + localeList.length);
+        for (Locale g: localeList) {
+            System.out.println(g);
+        }
     }
 
     // Create a generic English-speaking locale.
@@ -19,7 +32,23 @@ public class LocaleMain {
     // Create an English-speaking, Canadian locale.
     public void local2() {
         Locale locale = new Locale("en", "CA");
-        println(locale.getDisplayCountry());
+        String test = locale.getDisplayCountry();
+
+        // Get the current time in Hong Kong
+        Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("Hongkong"));
+        int hour12 = cal.get(Calendar.HOUR); // 0..11
+        int minutes = cal.get(Calendar.MINUTE); // 0..59
+        int seconds = cal.get(Calendar.SECOND); // 0..59
+        boolean am = cal.get(Calendar.AM_PM) == Calendar.AM;
+
+        println(hour12);
+        println(minutes);
+        println(seconds);
+        if(am) {
+            println("AM");
+        } else {
+            println("PM");
+        }
     }
 
     // Create a very specific English-speaking, U.S. locale
